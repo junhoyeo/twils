985fb743a06e42643e006401b6105903a9d6609a
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-04-15 20:48:56 +0900

    📋 feat: TWIL

---
+++ src/components/JunoTwil.vue
@@ -0,0 +1,61 @@
+<script>
+  export default { 
+    name: 'JunoTwil',
+    props: {
+      text: {
+        type: String
+      },
+      type: {
+        type: String
+      },
+      twils: {
+        type: Array,
+        required: false
+      },
+      nesting: {
+        type: Number,
+        default: 0
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="twil">
+    <template
+      v-if="text"
+    >
+      <li v-if="type === 'string'">
+        {{ text }}
+      </li>
+      <pre v-else-if="type === 'code'"><code>{{ text }}</code></pre>
+    </template>
+    <ul v-if="twils">
+      <li>
+        <juno-twil
+          v-for="(twil, idx) in twils" 
+          :key="`twil-${nesting}-${idx}`"
+          :text="twil.text"
+          :type="twil.type"
+          :twils="twil.twils"
+          :nesting="nesting + 1"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.twil {
+  font-size: 1.5rem;
+
+  pre {
+    width: 80%;
+    color: #282a36;
+  }
+
+  li {
+    color: #2A2B4A;
+  }
+}
+</style>
